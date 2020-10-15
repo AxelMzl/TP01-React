820e4ba3f30743e095a50893b6048795d4e40d49
unknown <axelmazel@hotmail.com>
2020-10-15 14:40:10 +0200

    Add video background for the homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Grommet, Box, Text } from 'grommet';
+import ReactDOM from 'react-dom';
+import VideoBg from "reactjs-videobg";
+
+import { Grommet, Heading, Box, Avatar, Video } from 'grommet';
+import { Notification, Home, ChatOption, UserFemale } from 'grommet-icons';
+
 import Name from './components/Name';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
+import HomePage from './components/Home';
 const theme = {
   global: {
     font: {
@@ -13,24 +18,22 @@ const theme = {
   },
 };
 
-function App() {
-  return (
-    <Grommet plain>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </Grommet>
-  );
-}
+const VideoBackground = (props) => (
+    <VideoBg>
+      <VideoBg.Source src="video.mp4"type="video/mp4" />
+    </VideoBg>
+);
 
-export default App;
+export default () => (
+    <Grommet theme={theme}>
+      <Box direction="column">
+        <Box direction="row">
+          <VideoBackground />
+        </Box>
+        <Box direction="row">
+          <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80"/>
+          <Heading margin="none">Axel Mazel</Heading>
+        </Box>
+      </Box>
+    </Grommet>
+);
---
+++ src/components/Navbar.js
@@ -0,0 +1,1 @@
+import { Header, Button, Menu} from 'grommet';
